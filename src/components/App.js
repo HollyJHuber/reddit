66d79e70a627262f604029390479a89cccb6f487
HollyJHuber <hollyjhuber@gmail.com>
2017-11-10 14:24:14 -0500

    update structure, reducers, index.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Everything in life is only for now! </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/// code copied from starships
+
+// import from these three libraries, which we installed with npm
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+// grab our reducer. import will automatically choose the index.js file in the
+// ./reducers folder.
+import reducer from './reducers'
+
+// Create the redux Store. This is the central hub of Redux. It keeps track of
+// the state (data) and calls the reducers.
+const store = createStore(
+    reducer,
+     applyMiddleware(
+        // thunk works behind the scenes to allow us to make async actions. Without
+        // it, the code in actions/index.js would not work.
+        thunkMiddleware,
+        createLogger() // just to help us see what's going on
+    )
+);
+
+// Trigger the fetchStarships "action"
+//store.dispatch(fetchStarships());
+
+// We need wrap our whole app in the Provider component. This lets Redux do its magic.
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
+
